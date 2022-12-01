2af333b67055379d2c6112b9d2479ed5ca29e97f
David Santos <davidprocode@proton.me>
2022-12-01 13:09:41 +0000

    add venon

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello World!")
+const venom = require('venom-bot');
+
+async function session (session_name) {
+    let session = await venom.create({ session: session_name });
+    return session
+}
+
+session('ti-ceep').then( client => client.onAnyMessage( message => console.log(message.body)))
+
